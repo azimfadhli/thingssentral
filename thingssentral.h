9f361b19391c25d23393054a71790c0ea7371395
AzimFadhli <azimfadhli.uni@gmail.com>
2024-12-12 01:45:47 +0800

    1st use of class

--- thingssentral.h
+++ thingssentral.h
@@ -1,95 +1,91 @@
-/**
- * @file
- *  need to define variable USERID
- * 
- * Example to read node;
- *    readNode(NodeID);
- *    readNode(NodeID, repeat);  //repeat 0-false  or 1-true
- * 
- * 
- * Example to post node;
- *    sendNode(NodeID, valueToSent);
- *    sendNode(NodeID, valueToSent, NodeID, valueToSent, NodeID, valueToSent, NodeID, valueToSent); 
- *    // can sent up to 4 node id.
- * 
- */
-
-
-#ifndef USERID
-#define USERID "069000"
-#endif
-
+#pragma once
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-/*--------------------------------------------------*/
-/*---------------- GET DATA FROM TS ----------------*/
-/*--------------------------------------------------*/
+// for later update
+/* #if defined(ESP8266)
 
-//FUNCTION DEFINITION
-String readNode(String __NodeID, bool _repeat = false);
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
+#error "This board is not supported."
+#endif*/
 
-// THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID (STRING).
-String ICACHE_RAM_ATTR readNode(String __NodeID, bool _repeat) {
-  HTTPClient http;
-  //WiFiClient client;
-  String completedLink = String("http://thingssentral.io/ReadNode?Params=tokenid|")
-                         + USERID + "@NodeId|" + __NodeID;
-  
-  http.begin(completedLink);
-  http.GET();
-  String _reply = http.getString();
-  http.end();
+class thingssentral {
+public:
+  String userid = "0069101";
 
-  if (_reply == "") {
-    Serial.println("BEBUG: error bro: " + __NodeID);
-    if (_repeat == false) {
-      return "e";
-    } else {
-      WiFi.disconnect();
-      delay(1000);
-      //wifiConnect();
-      delay(1000);
-      return readNode(__NodeID, true);
+  /*--------------------------------------------------*/
+  /*---------------- GET DATA FROM TS ----------------*/
+  /*--------------------------------------------------*/
+
+  //FUNCTION DEFINITION
+  //String readNode(String __NodeID, bool _repeat = false);
+
+  // THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID (STRING).
+  String ICACHE_RAM_ATTR readNode(String __NodeID, bool _repeat = false) {
+    HTTPClient http;
+    //WiFiClient client;
+    String completedLink = String("http://thingssentral.io/ReadNode?Params=tokenid|")
+                           + userid + "@NodeId|" + __NodeID;
+
+    http.begin(completedLink);
+    http.GET();
+    String _reply = http.getString();
+    http.end();
+
+    if (_reply == "") {
+      Serial.println("BEBUG: error bro: " + __NodeID);
+      if (_repeat == false) {
+        return "e";
+      } else {
+        WiFi.disconnect();
+        delay(1000);
+        //wifiConnect();
+        delay(1000);
+        return readNode(__NodeID, true);
+      }
     }
+    //Serial.print("BEBUG: Full reply for: " + __NodeID + "\t==" + _reply + "==\t\t>");
+    //Serial.println(_reply.substring(__NodeID.length() + 1, __NodeID.length() + 2));
+    return _reply.substring(__NodeID.length() + 1, __NodeID.length() + 2);
   }
-  //Serial.print("BEBUG: Full reply for: " + __NodeID + "\t==" + _reply + "==\t\t>");
-  //Serial.println(_reply.substring(__NodeID.length() + 1, __NodeID.length() + 2));
-  return _reply.substring(__NodeID.length() + 1, __NodeID.length() + 2);
-}
 
 
-/*--------------------------------------------------*/
-/*----------------- POST DATA TO TS ----------------*/
-/*--------------------------------------------------*/
+  /*--------------------------------------------------*/
+  /*----------------- POST DATA TO TS ----------------*/
+  /*--------------------------------------------------*/
 
-//FUNCTION DEFINITION
-String sendNode(String _NodeID1, String _Data1,
-                String _NodeID2 = "", String _Data2 = "",   //OPTIONAL PARAM
-                String _NodeID3 = "", String _Data3 = "",   //OPTIONAL PARAM
-                String _NodeID4 = "", String _Data4 = "");  //OPTIONAL PARAM
+  //FUNCTION DEFINITION
+  /* String sendNode(String _NodeID1, String _Data1,
+                  String _NodeID2 = "", String _Data2 = "",   //OPTIONAL PARAM
+                  String _NodeID3 = "", String _Data3 = "",   //OPTIONAL PARAM
+                  String _NodeID4 = "", String _Data4 = "");  //OPTIONAL PARAM
+ */
 
+  // THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID AND ITS RESPECTIVE DATA.
+  String ICACHE_RAM_ATTR sendNode(String _NodeID1, String _Data1,
+                                  String _NodeID2 = "", String _Data2 = "",  //OPTIONAL PARAM
+                                  String _NodeID3 = "", String _Data3 = "",  //OPTIONAL PARAM
+                                  String _NodeID4 = "", String _Data4 = "")  //OPTIONAL PARAM
+  {
+    String completedLink = String("http://thingssentral.io/postlong?data=userid|") + userid;
+    completedLink.concat(String("@") + _NodeID1 + "|" + String(_Data1));
+    if (_NodeID2 != "") completedLink.concat(String("@") + _NodeID2 + "|" + String(_Data2));
+    if (_NodeID3 != "") completedLink.concat(String("@") + _NodeID3 + "|" + String(_Data3));
+    if (_NodeID4 != "") completedLink.concat(String("@") + _NodeID4 + "|" + String(_Data4));
 
-// THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID AND ITS RESPECTIVE DATA.
-String ICACHE_RAM_ATTR sendNode(String _NodeID1, String _Data1,
-                String _NodeID2, String _Data2,  //OPTIONAL PARAM
-                String _NodeID3, String _Data3,  //OPTIONAL PARAM
-                String _NodeID4, String _Data4)  //OPTIONAL PARAM
-{
-  String completedLink = String("http://thingssentral.io/postlong?data=userid|") + USERID;
-  completedLink.concat(String("@") + _NodeID1 + "|" + String(_Data1));
-  if (_NodeID2 != "") completedLink.concat(String("@") + _NodeID2 + "|" + String(_Data2));
-  if (_NodeID3 != "") completedLink.concat(String("@") + _NodeID3 + "|" + String(_Data3));
-  if (_NodeID4 != "") completedLink.concat(String("@") + _NodeID4 + "|" + String(_Data4));
+    HTTPClient http;
 
-  HTTPClient http;
+    http.begin(completedLink);
+    http.GET();
+    String _reply = http.getString();
+    http.end();
 
-  http.begin(completedLink);
-  http.GET();
-  String _reply = http.getString();
-  http.end();
+    Serial.println("Response: " + _reply);
 
-  Serial.println("Response: " + _reply);
-
-  return _reply;
-}
+    return _reply;
+  }
+};
--- thingssentral.ino
+++ thingssentral.ino
@@ -1,95 +1,47 @@
-/**
- * @file
- *  need to define variable USERID
- * 
- * Example to read node;
- *    readNode(NodeID);
- *    readNode(NodeID, repeat);  //repeat 0-false  or 1-true
- * 
- * 
- * Example to post node;
- *    sendNode(NodeID, valueToSent);
- *    sendNode(NodeID, valueToSent, NodeID, valueToSent, NodeID, valueToSent, NodeID, valueToSent); 
- *    // can sent up to 4 node id.
- * 
- */
+// thsi is an example immplementing "thingssentral.h"
 
+#include "thingssentral.h"
 
-#ifndef USERID
-#define USERID "069000"
-#endif
+// create object TS
+thingssentral TS;
 
-#include <WiFi.h>
-#include <HTTPClient.h>
+// change the wifi parameter
+const char *MainNetwork_SSID = "MyUiTM";
+const char *MainNetwork_PASS = "12345678";
 
-/*--------------------------------------------------*/
-/*---------------- GET DATA FROM TS ----------------*/
-/*--------------------------------------------------*/
+bool num = 0;
+void connectwifi();
 
-//FUNCTION DEFINITION
-String readNode(String __NodeID, bool _repeat = false);
+void setup() {
+  Serial.begin(115200);
 
-// THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID (STRING).
-String ICACHE_RAM_ATTR readNode(String __NodeID, bool _repeat) {
-  HTTPClient http;
-  //WiFiClient client;
-  String completedLink = String("http://thingssentral.io/ReadNode?Params=tokenid|")
-                         + USERID + "@NodeId|" + __NodeID;
-  
-  http.begin(completedLink);
-  http.GET();
-  String _reply = http.getString();
-  http.end();
+  // set the user Id for things sentral
+  TS.userid = "0069101";
 
-  if (_reply == "") {
-    Serial.println("BEBUG: error bro: " + __NodeID);
-    if (_repeat == false) {
-      return "e";
-    } else {
-      WiFi.disconnect();
-      delay(1000);
-      //wifiConnect();
-      delay(1000);
-      return readNode(__NodeID, true);
-    }
-  }
-  //Serial.print("BEBUG: Full reply for: " + __NodeID + "\t==" + _reply + "==\t\t>");
-  //Serial.println(_reply.substring(__NodeID.length() + 1, __NodeID.length() + 2));
-  return _reply.substring(__NodeID.length() + 1, __NodeID.length() + 2);
+  connectwifi();
 }
 
 
-/*--------------------------------------------------*/
-/*----------------- POST DATA TO TS ----------------*/
-/*--------------------------------------------------*/
+void loop() {
 
-//FUNCTION DEFINITION
-String sendNode(String _NodeID1, String _Data1,
-                String _NodeID2 = "", String _Data2 = "",   //OPTIONAL PARAM
-                String _NodeID3 = "", String _Data3 = "",   //OPTIONAL PARAM
-                String _NodeID4 = "", String _Data4 = "");  //OPTIONAL PARAM
+  Serial.print("read testnode: ");
+  Serial.println(TS.readNode("testnode"));
+
+  delay(3000);
+
+  TS.sendNode("testnode", String(num));
+
+  num = !num;
+}
 
 
-// THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID AND ITS RESPECTIVE DATA.
-String ICACHE_RAM_ATTR sendNode(String _NodeID1, String _Data1,
-                String _NodeID2, String _Data2,  //OPTIONAL PARAM
-                String _NodeID3, String _Data3,  //OPTIONAL PARAM
-                String _NodeID4, String _Data4)  //OPTIONAL PARAM
-{
-  String completedLink = String("http://thingssentral.io/postlong?data=userid|") + USERID;
-  completedLink.concat(String("@") + _NodeID1 + "|" + String(_Data1));
-  if (_NodeID2 != "") completedLink.concat(String("@") + _NodeID2 + "|" + String(_Data2));
-  if (_NodeID3 != "") completedLink.concat(String("@") + _NodeID3 + "|" + String(_Data3));
-  if (_NodeID4 != "") completedLink.concat(String("@") + _NodeID4 + "|" + String(_Data4));
-
-  HTTPClient http;
-
-  http.begin(completedLink);
-  http.GET();
-  String _reply = http.getString();
-  http.end();
-
-  Serial.println("Response: " + _reply);
-
-  return _reply;
+void connectwifi() {
+  WiFi.mode(WIFI_AP);
+  WiFi.begin(MainNetwork_SSID, MainNetwork_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.printf("Wifi connedted, IP");
+  Serial.println(WiFi.localIP());
 }
