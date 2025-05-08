abc796b31b0deed29268f2af2f31364574b4948d
AzimFadhli <azimfadhli.uni@gmail.com>
2025-05-08 21:32:35 +0800

    version 2
    ESP32 n ESP8266
    
    tested, OK!

--- thingssentral.h
+++ thingssentral.h
@@ -1,91 +1,82 @@
+String TSuserID = "00123";
+String APIlinkRead = "http://thingssentral.io/ReadNode?Params=tokenid|" + TSuserID + "@NodeId|";
+String APIlinkSent = "http://thingssentral.io/postlong?data=userid|" + TSuserID;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #pragma once
-#include <WiFi.h>
-#include <HTTPClient.h>
-
-// for later update
-/* #if defined(ESP8266)
-
 #if defined(ESP32)
 #include <WiFi.h>
+#include <HTTPClient.h>
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #else
 #error "This board is not supported."
-#endif*/
+#endif
 
-class thingssentral {
-public:
-  String userid = "0069101";
+String IRAM_ATTR GET(String completedLink) {
+  HTTPClient http;
 
-  /*--------------------------------------------------*/
-  /*---------------- GET DATA FROM TS ----------------*/
-  /*--------------------------------------------------*/
+#ifdef ESP32
+  http.begin(completedLink);
+#else
+  WiFiClient client;
+  http.begin(client, completedLink);
+#endif
 
-  //FUNCTION DEFINITION
-  //String readNode(String __NodeID, bool _repeat = false);
+  http.GET();
+  String reply = http.getString();
+  http.end();
+  return reply;
+}
 
-  // THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID (STRING).
-  String ICACHE_RAM_ATTR readNode(String __NodeID, bool _repeat = false) {
-    HTTPClient http;
-    //WiFiClient client;
-    String completedLink = String("http://thingssentral.io/ReadNode?Params=tokenid|")
-                           + userid + "@NodeId|" + __NodeID;
 
-    http.begin(completedLink);
-    http.GET();
-    String _reply = http.getString();
-    http.end();
 
-    if (_reply == "") {
-      Serial.println("BEBUG: error bro: " + __NodeID);
-      if (_repeat == false) {
-        return "e";
-      } else {
-        WiFi.disconnect();
-        delay(1000);
-        //wifiConnect();
-        delay(1000);
-        return readNode(__NodeID, true);
-      }
-    }
-    //Serial.print("BEBUG: Full reply for: " + __NodeID + "\t==" + _reply + "==\t\t>");
-    //Serial.println(_reply.substring(__NodeID.length() + 1, __NodeID.length() + 2));
-    return _reply.substring(__NodeID.length() + 1, __NodeID.length() + 2);
+/*--------------------------------------------------*/
+/*---------------- GET DATA FROM TS ----------------*/
+/*--------------------------------------------------*/
+// THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID (STRING).
+String IRAM_ATTR readNode(String __NodeID, bool _fullReply = false) {
+  String _reply = GET(APIlinkRead + __NodeID);
+  if (_reply == "") {
+    Serial.println("thingssentral.h: error, blank reply from server. Check connection. : " + __NodeID);
+    return "error";
   }
 
+  String _valueOnly = _reply.substring(_reply.indexOf("|") + 1,
+                                       _reply.indexOf("|", _reply.indexOf("|") + 1));
+  Serial.println("thingssentral.h: readNode: " + _valueOnly);
 
-  /*--------------------------------------------------*/
-  /*----------------- POST DATA TO TS ----------------*/
-  /*--------------------------------------------------*/
-
-  //FUNCTION DEFINITION
-  /* String sendNode(String _NodeID1, String _Data1,
-                  String _NodeID2 = "", String _Data2 = "",   //OPTIONAL PARAM
-                  String _NodeID3 = "", String _Data3 = "",   //OPTIONAL PARAM
-                  String _NodeID4 = "", String _Data4 = "");  //OPTIONAL PARAM
- */
-
-  // THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID AND ITS RESPECTIVE DATA.
-  String ICACHE_RAM_ATTR sendNode(String _NodeID1, String _Data1,
-                                  String _NodeID2 = "", String _Data2 = "",  //OPTIONAL PARAM
-                                  String _NodeID3 = "", String _Data3 = "",  //OPTIONAL PARAM
-                                  String _NodeID4 = "", String _Data4 = "")  //OPTIONAL PARAM
-  {
-    String completedLink = String("http://thingssentral.io/postlong?data=userid|") + userid;
-    completedLink.concat(String("@") + _NodeID1 + "|" + String(_Data1));
-    if (_NodeID2 != "") completedLink.concat(String("@") + _NodeID2 + "|" + String(_Data2));
-    if (_NodeID3 != "") completedLink.concat(String("@") + _NodeID3 + "|" + String(_Data3));
-    if (_NodeID4 != "") completedLink.concat(String("@") + _NodeID4 + "|" + String(_Data4));
-
-    HTTPClient http;
-
-    http.begin(completedLink);
-    http.GET();
-    String _reply = http.getString();
-    http.end();
-
-    Serial.println("Response: " + _reply);
-
+  if (_fullReply) {
     return _reply;
+  } else {
+    return _valueOnly;
   }
-};
+}
+
+
+/*--------------------------------------------------*/
+/*----------------- POST DATA TO TS ----------------*/
+/*--------------------------------------------------*/
+// THIS FUNC WILL RETURN THE RESPONSE (STRING) FOR THE GIVEN NODE ID AND ITS RESPECTIVE DATA.
+String IRAM_ATTR sendNode(String _NodeID1, String _Data1,
+                          String _NodeID2 = "", String _Data2 = "",  //OPTIONAL PARAM
+                          String _NodeID3 = "", String _Data3 = "",  //OPTIONAL PARAM
+                          String _NodeID4 = "", String _Data4 = "")  //OPTIONAL PARAM
+{
+  String completedLink = APIlinkSent;
+  completedLink.concat(String("@") + _NodeID1 + "|" + String(_Data1));
+  if (_NodeID2 != "") completedLink.concat(String("@") + _NodeID2 + "|" + String(_Data2));
+  if (_NodeID3 != "") completedLink.concat(String("@") + _NodeID3 + "|" + String(_Data3));
+  if (_NodeID4 != "") completedLink.concat(String("@") + _NodeID4 + "|" + String(_Data4));
+
+  String _reply = GET(completedLink);
+  if (_reply == "") {
+    Serial.println("thingssentral.h: error, blank reply from server. Check connection.");
+    return "error";
+  }
+
+  Serial.println("thingssentral.h: sendNode: " + _reply);
+  return _reply;
+}
