444ef8ebf9f159c797fca45a076035ca25fd8f63
AzimFadhli <azimfadhli.uni@gmail.com>
2025-05-08 21:20:15 +0800

    version 2
    ESP32 n ESP8266
    
    tested, OK!

--- thingssentral.ino
+++ thingssentral.ino
@@ -1,43 +1,36 @@
-// thsi is an example immplementing "thingssentral.h"
-
 #include "thingssentral.h"
 
-// create object TS
-thingssentral TS;
-
-// change the wifi parameter
-const char *MainNetwork_SSID = "MyUiTM";
-const char *MainNetwork_PASS = "12345678";
-
-bool num = 0;
-void connectwifi();
+String ssid = "MyThingsSentral";
+String pass = "12345678";
+String nodeID = "001231010101";
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  connectwifi();
-}
 
-
-void loop() {
-
-  Serial.print("read testnode: ");
-  Serial.println(TS.readNode("testnode"));
-
-  delay(3000);
-
-  TS.sendNode("testnode", String(num));
-
-  num = !num;
-}
-
-
-void connectwifi() {
-  WiFi.mode(WIFI_AP);
-  WiFi.begin(MainNetwork_SSID, MainNetwork_PASS);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  Serial.println(".");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.printf("Wifi connedted, IP");
+  Serial.printf("Wifi connected, IP ");
   Serial.println(WiFi.localIP());
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(APIlinkRead);
+  Serial.println(readNode(nodeID));
+  Serial.println(APIlinkSent);
+  sendNode(nodeID, "123");
+
+  delay(5000);
+
+  Serial.println(readNode(nodeID));
+  sendNode(nodeID, "456");
+
+  delay(5000);
 }
